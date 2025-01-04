0b004ee6a93802bba737838a654aafa0076c2a67
Dmitry Solomakha <solomakhadmitry@gmail.com>
2025-01-04 20:46:40 +0200

    Created tabBarView for visualisation TabBarController. Added some methods for setup View and constraints.

--- GoalTime/MainScreenViewController.swift
+++ GoalTime/MainScreenViewController.swift
@@ -8,16 +8,33 @@
 import UIKit
 
 class MainScreenViewController: UIViewController {
-
     weak var coordinator: Coordinator?
+    let tabBarView = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let customColor = UIColor(named: "backgroundColor") {
-            view.backgroundColor = customColor
-        } else {
-            view.backgroundColor = UIColor.systemBlue // Fallback color
-        }
+        setupView()
+        addConstraints()
         // Do any additional setup after loading the view.
+    }
+    
+    func setupView(){
+        let customColor = UIColor(named: "backgroundColor")
+        view.backgroundColor = customColor
+        
+        view.addSubview(tabBarView)
+        tabBarView.backgroundColor = .quaternarySystemFill
+        tabBarView.translatesAutoresizingMaskIntoConstraints = false
+        tabBarView.layer.cornerRadius = 30
+    }
+    
+    func addConstraints() {
+        NSLayoutConstraint.activate([
+            tabBarView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -30),
+            tabBarView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -30),
+            tabBarView.heightAnchor.constraint(equalToConstant: 60),
+            tabBarView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        ])
     }
     
 }
